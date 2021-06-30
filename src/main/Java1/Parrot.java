2d058c62c956613dbbecf032c80dd28781fcbdbb
IliaMer <imerzliak@gmail.com>
2021-07-01 00:07:26 +0300

    добавил две директории: main/java1 test/java1
    добавил новые классы:
    Parrot
    TestParrot

---
+++ src/main/Java1/Parrot.java
@@ -0,0 +1,27 @@
+package main.Java1;
+
+public class Parrot {
+    public String name;
+    public String color;
+    public int length;
+    public static String breed;
+    public int wingspan;
+    public double weight;
+
+    public void talk() {
+        System.out.println("В среднем птица может запомнить более 1500 слов.");
+    }
+
+    public static void fly() {
+        System.out.println("Отлично маневрируют в полете, т.к. Обитают жако в скученных и крупных лесах.");
+    }
+
+    public void eat() {
+        System.out.println("Жако питаются плодами пальмового дерева, семенами, фруктами, лиственной массой, а также были замечены в поедании улиток/");
+    }
+
+    public void sleep() {
+        System.out.println("Жако спят 10-12 часов");
+    }
+}
+
---
+++ src/test/Java1/TestParrot.java
@@ -0,0 +1,36 @@
+package test.Java1;
+
+import main.Java1.Parrot;
+
+public class TestParrot {
+
+    public static void main(String[] args) {
+        Parrot jaco1 = new Parrot();
+
+        jaco1.name = "Гришаня";
+        jaco1.color = "Пепельно-серый";
+        jaco1.length = 40;
+        jaco1.wingspan = 22;
+        jaco1.weight = 550.0;
+
+
+
+        Parrot jaco2 = new Parrot();
+
+        jaco2.name = "Игорёк";
+        jaco2.color = "Пурпурно-красный";
+        jaco2.length = 45;
+        jaco2.wingspan = 20;
+        jaco2.weight = 600.0;
+
+        System.out.println("Первый попугай " + jaco1.name + ", раскрас у него " + jaco1.color + ", весит он " + jaco1.weight + ", его длина " + jaco1.length + ", а размах крыльев " + jaco1.wingspan);
+        System.out.println("Второй попугай " + jaco2.name + ", раскрас у него " + jaco2.color + ", весит он " + jaco2.weight + ", его длина " + jaco2.length + ", а размах крыльев " + jaco2.wingspan);
+
+        jaco1.talk();
+        jaco1.eat();
+        jaco2.sleep();
+        jaco2.fly();
+
+    }
+
+}
